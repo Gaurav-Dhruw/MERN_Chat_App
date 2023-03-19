f2fa4cfdbc2f5482241d11b86c28bd8dda8c01e9
Gaurav Dhruw <dhruwgaurav01@gmail.com>
2023-03-19 17:44:18 +0530

    vite config changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port:'5000'
-  }
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
 })
