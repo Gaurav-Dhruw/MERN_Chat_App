9ca5d4a0c1e717723dc4e9d91a9ef46300bf013f
Gaurav Dhruw <dhruwgaurav01@gmail.com>
2023-03-20 15:50:55 +0530

    client side url updated

--- client/src/lib/axios.config.js
+++ client/src/lib/axios.config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:8000",
+  baseURL: "https://chatico.onrender.com",
   headers: {
     "Content-type": "application/json"
   }
